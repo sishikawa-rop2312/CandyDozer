e4824e4c5a6da5560efc8359da0ae44b8ba92983
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-05-15 18:46:02 +0900

    教科書P.188まで実装

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject candyPrefab;
+    public float shotForce;
+    public float shotTorque;
+
+    void Update()
+    {
+
+    }
+}
