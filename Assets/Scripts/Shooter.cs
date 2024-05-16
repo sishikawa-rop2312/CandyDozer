4d0e661e41138131ad463ca6632f737c002a55fd
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-05-16 18:41:28 +0900

    完成

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
+
+    int shotPower = MaxShotPower;
+    AudioSource shotSound;
+
     public GameObject[] candyPrefabs;
     public Transform candyParentTransform;  // 空Emptyの「Candies」を設定する（Transform型なのに注意）
     public CandyManager candyManager;
     public float shotForce;
     public float shotTorque;
     public float baseWidth; // オブジェクトBaseの幅が5なので、画面側で5を設定
+
+    void Start()
+    {
+        shotSound = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
@@ -36,6 +47,7 @@ public class Shooter : MonoBehaviour
     {
         // キャンディを生成できる条件外ならShotしない
         if (candyManager.GetCandyAmount() <= 0) return;
+        if (shotPower <= 0) return;
 
         // プレハブからCandyオブジェクトを生成
         // Quaternion.identityとは回転なしの状態（Rotationが0, 0, 0の状態）
@@ -55,5 +67,36 @@ public class Shooter : MonoBehaviour
 
         // キャンディを消費
         candyManager.ConsumeCandy();
+
+        //  ShotPowerを消費
+        ConsumePower();
+
+        // サウンド再生
+        shotSound.Play();
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        // ShotPowerの残数を+の数で表示
+        string label = "";
+        for (int i = 0; i < shotPower; i++) label = label + "+";
+
+        GUI.Label(new Rect(50, 65, 100, 30), label);
+    }
+
+    void ConsumePower()
+    {
+        //ShotPowerを消費すると同時に回復のカウントをスタート
+        shotPower--;
+        StartCoroutine(RecoverPower());
+    }
+
+    IEnumerator RecoverPower()
+    {
+        // 一定秒数待った後にshotPowerを回復
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
