b85e213dcaef44a9b8c025cb4a6c69cf1359531d
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-05-16 16:19:58 +0900

    P207まで実装

--- Assets/Scripts/CandyManager.cs
+++ Assets/Scripts/CandyManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CandyManager : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int RecoverySeconds = 10;
 
     // 現在のキャンディストック数
     public int candy = DefaultCandyAmount;
+
+    // ストック回復までの残り秒数
+    int counter;
 
     public void ConsumeCandy()
     {
@@ -31,6 +35,31 @@ public class CandyManager : MonoBehaviour
         // キャンディのストック数を表示
         string label = "Candy：" + candy;
 
+        // 回復カウントしている時だけ秒数を表示
+        if (counter > 0) label = label + "（" + counter + "s）";
+
         GUI.Label(new Rect(50, 50, 100, 30), label);
+    }
+
+    void Update()
+    {
+        // キャンディストックがデフォルトより少なく、回復カウントをしていないときにカウントをスタートさせる
+        if (candy < DefaultCandyAmount && counter <= 0)
+        {
+            StartCoroutine(RecoverCandy());
+        }
+
+        IEnumerator RecoverCandy()
+        {
+            counter = RecoverySeconds;
+
+            // 1秒ずつカウントを進める
+            while (counter > 0)
+            {
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            candy++;
+        }
     }
 }
